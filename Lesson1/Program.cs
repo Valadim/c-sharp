c589ce2325fd34fa75333611128a0078b3516434
Vadim <sakeyev@mail.ru>
2023-07-09 15:03:31 +0600

    task 6 ready

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -20,7 +20,6 @@ else
   min = a;
 }
 
-
 Console.WriteLine($"Большее число: {max}");
 Console.WriteLine($"Меньшее число: {min}");
 
@@ -48,3 +47,18 @@ else {
 if (a > max) max = a;
 
 Console.WriteLine($"Максимальное число: {max}");
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+  Console.WriteLine("Число является четным.");
+}
+else
+{
+  Console.WriteLine("Число не является четным.");
+}
