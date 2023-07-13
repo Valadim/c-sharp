d91d411516101e1395487e9bf9289f63ba9b9b3d
Vadim <sakeyev@mail.ru>
2023-07-13 18:22:04 +0600

    leasson 2 tasks ready

---
+++ Lesson2/Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+int thirdDigit = (number / 100) % 10;
+
+if (thirdDigit != 0)
+{
+  Console.WriteLine("Третья цифра числа: " + thirdDigit);
+}
+else
+{
+  Console.WriteLine("У числа нет третьей цифры.");
+}
---
+++ Lesson2/Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите цифру дня недели (1-7):");
+int dayOfWeek = int.Parse(Console.ReadLine());
+
+if (dayOfWeek >= 1 && dayOfWeek <= 7)
+{
+  bool isWeekend = (dayOfWeek == 6 || dayOfWeek == 7);
+
+  if (isWeekend)
+  {
+    Console.WriteLine("День является выходным.");
+  }
+  else
+  {
+    Console.WriteLine("День не является выходным.");
+  }
+}
+else
+{
+  Console.WriteLine("Некорректный номер дня недели. Введите число от 1 до 7.");
+}
