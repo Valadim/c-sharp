4f4c0f7c92e968dcaf2754687fef93eb59d7b665
Vadim <sakeyev@mail.ru>
2023-07-18 15:24:45 +0600

    Lesson3 Task23 ready

---
+++ Lesson3/Task19/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+ Console.Write("Введите пятизначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (IsPalindrome(number))
+        {
+            Console.WriteLine("Данное число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Данное число не является палиндромом.");
+        }
+
+        Console.ReadLine();
+    }
+
+   bool IsPalindrome(int number)
+    {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0)
+        {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
---
+++ Lesson3/Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+GetCube(num);
+
+void GetCube (int n)
+{
+
+  for (int i = 1; i <= n; i++)
+  {
+    Console.WriteLine($"Куб числа {i} = {i * i * i}");
+  }
+
+}
