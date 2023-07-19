347aabca56e7c5fb0646d215e9b9cbae96ef6f52
Vadim <sakeyev@mail.ru>
2023-07-19 22:17:52 +0600

    Lesson4 Tasks ready

---
+++ Lesson4/Task25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int GetMultiply(int x, int y)
+{
+
+  int result = 1;
+
+  for (int i = 0; i < y; i++)
+  {
+    result = result * x;
+  }
+
+  return result;
+
+}
+
+int temp = GetMultiply(num1, num2);
+
+Console.WriteLine($"Число {num1} в степени {num2} равно {temp}");
+
---
+++ Lesson4/Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digitSum = DigitSum(number);
+Console.WriteLine($"Сумма цифр числа {number} равна {digitSum}");
+
+
+int DigitSum(int number)
+{
+    int sum = 0;
+
+    while (number != 0)
+    {
+        int remainder = number % 10;
+        sum = sum + remainder;
+        
+        number /= 10;
+    }
+
+    return sum;
+}
+
+
