c26d17f0ef79003695527a1c0d72953972cb912c
Vadim <sakeyev@mail.ru>
2023-07-31 00:52:08 +0600

    Lesson7 Tasks Ready

---
+++ Lesson7/Task47/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] array = new double[5, 4];
+
+FillDoubleArray(array, -10.0);
+
+PrintMultiplyArray(array);
+
+double[,] FillDoubleArray(double[,] arr, double minValue = 1.0, double maxValue = 10.0, int round = 1)
+{
+  Random random = new Random();
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = Math.Round(random.NextDouble() * (maxValue - minValue) + minValue, round);
+    }
+  }
+
+  return arr;
+}
+
+void PrintMultiplyArray(double[,] arr) {
+
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
---
+++ Lesson7/Task50/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого элемента в массиве нет
+
+int[,] array = { { 1, 4, 7, 2 },
+                 { 5, 9, 2, 3 },
+                 { 8, 4, 2, 4 }
+               };
+
+
+GetElementFromMultipleArray(array);
+
+void GetElementFromMultipleArray(int[,] arr)
+{
+
+  Console.Write("Введите координат X:");
+  int xCoord = Convert.ToInt32(Console.ReadLine());
+
+  Console.Write("Введите координат Y:");
+  int yCoord = Convert.ToInt32(Console.ReadLine());
+
+  if (xCoord > arr.GetLength(0) || yCoord > arr.GetLength(1))
+  {
+    Console.WriteLine("Такого элемента в массиве нет");
+  }
+  else
+  {
+    Console.WriteLine($"Значение элемента: {arr[xCoord, yCoord]}");
+  }
+}
+
